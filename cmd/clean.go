fb375afcb57346ebea9502653f12d102384ead8f
XDwanj <xdwanj@qq.com>
2024-04-11 22:30:14 +0800

    fix

--- cmd/clean.go
+++ cmd/clean.go
@@ -10,7 +10,7 @@ import (
 var cleanCmd = &cobra.Command{
 	Use:    "clean",
 	Short:  "清理 Lutris 中 Gsgm 游戏",
-	Args:   cobra.MinimumNArgs(1),
+	Args:   cobra.NoArgs,
 	PreRun: InitLog,
 	Run: func(cmd *cobra.Command, args []string) {
 		contro.CleanAction()
